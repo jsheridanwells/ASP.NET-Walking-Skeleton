c9da2eed9f6a3ee7067f8c18733812c31bf1fabe
Jeremy Wells <jeremysheridanwells@Jeremys-MacBook-Pro.local>
2018-02-10 12:21:35 -0600

    set up Values model and controller

---
+++ Data/DataContext.cs
@@ -0,0 +1,11 @@
+using Skeleton.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Skeleton.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}    
+        public DbSet<Value> Values { get; set; }
+    }
+}
